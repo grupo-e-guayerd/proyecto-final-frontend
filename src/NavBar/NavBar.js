1cdd8dabb6e542b27b2b74403670ec78d2a13bf8
Matias Aquino <m4tias.a@gmail.com>
2020-11-13 18:24:28 -0300

    navbar done

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { NavLink } from "react-router-dom";
+
+class NavBar extends Component {
+    render() {
+        return (
+            <>
+                {/* NAVBAR COMPONENT */}
+                <nav className="navbar">
+                    {/* NAVBAR TITLE */}
+                    <div className="navbar-title">
+                        <h1>TITULO</h1>
+                    </div>
+
+                    {/* NAVBAR LINKS */}
+                    <div className="navbar-links">
+                        <NavLink exact to="/" className="navbar-navlink">Home</NavLink>
+                        <NavLink exact to="/servicios" className="navbar-navlink">Servicios</NavLink>
+                        <NavLink exact to="/nosotros" className="navbar-navlink">Nosotros</NavLink>
+                    </div>
+                </nav>
+            </>
+        );
+    }
+}
+
+export default NavBar;
