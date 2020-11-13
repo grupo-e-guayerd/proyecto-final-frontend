e8d0d8bf44a9af303eaf271fa9b9e5652a139576
Guido <alferezguido@outlook.com>
2020-11-13 19:23:44 -0300

    Carousel: Done!

---
+++ src/Home/Carousel/Carousel.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+
+export default class Banner extends React.Component{
+    render (){
+        return( 
+          <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src=""
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <div>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src=""
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src=""
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Third slide label</h3>
+                  <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+        )
+    }
+}
+
---
+++ src/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Carousel from "./Carousel/.js"
+
+export default class Home extends React.Component{
+    render (){
+        return( 
+            <Carousel></Carousel>
+        )
+    }
+}
