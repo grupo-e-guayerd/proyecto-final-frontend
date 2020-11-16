969032a220cd2dfc04e89314511b44e36ac64da2
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-11-15 23:24:07 -0300

    Un breve desarrollo.

--- src/Home/Ranking/Ranking.js
+++ src/Home/Ranking/Ranking.js
@@ -1,10 +1,27 @@
 import React from "react"
+import CardList from "../../CardList/CardList"
 
-export default class Ranking extends React.Component{
+export default class Ranking extends React.Component 
+{
+    constructor() {
+        super();
+        this.state = {
+            arrayMostPopulars: []
+        }
+    }
+
+    componentDidMount()
+    {
+        /* TODO Request data from the server. 
+            Se van pedir los profesionales registrados y se van a agregar al arreglo del estado los (por ejemplo)
+            20 con mejor ranking, */
+    }
+
     render (){
-        return(
+        return (
             <>
-                <h2></h2>
+                <h2>{/* TODO */}</h2>
+                <CardList arrayMostPopulars={this.state.arrayMostPopulars} />
             </>
         )
     }
