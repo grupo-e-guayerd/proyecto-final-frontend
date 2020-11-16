6c1beb76ab30da5f01aa6e38b359f287df112e19
Nahuel <nahuel.trucco.bkn@gmail.com>
2020-11-16 02:25:07 -0300

    Se prueba <Ranking>

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Banner from "./Carousel/Banner.js"
+import Ranking from "./Ranking/Ranking"
 
 export default class Home extends React.Component{
     render (){
         return( 
             <div>
-                <Banner></Banner> <br/><br/>
+                <Banner />
+                <Ranking />
             </div>
         )
     }
